f75f4b206bd1279ec0e143b43801e2a3accc773e
Abdullah <saifabu108@gmail.com>
2025-10-27 19:56:50 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

---
+++ src/firebase/provider.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { type ReactNode, createContext, useContext } from 'react';
+import type { FirebaseApp } from 'firebase/app';
+import type { Auth } from 'firebase/auth';
+import type { Firestore } from 'firebase/firestore';
+
+type FirebaseContextValue = {
+  firebaseApp: FirebaseApp | null;
+  auth: Auth | null;
+  firestore: Firestore | null;
+};
+
+const FirebaseContext = createContext<FirebaseContextValue>({
+  firebaseApp: null,
+  auth: null,
+  firestore: null,
+});
+
+export function FirebaseProvider({
+  children,
+  firebaseApp,
+  auth,
+  firestore,
+}: {
+  children: ReactNode;
+  firebaseApp: FirebaseApp | null;
+  auth: Auth | null;
+  firestore: Firestore | null;
+}) {
+  return (
+    <FirebaseContext.Provider value={{ firebaseApp, auth, firestore }}>
+      {children}
+    </FirebaseContext.Provider>
+  );
+}
+
+export const useFirebase = () => useContext(FirebaseContext);
+
+export const useFirebaseApp = () => {
+  const context = useContext(FirebaseContext);
+  if (context === undefined) {
+    throw new Error('useFirebaseApp must be used within a FirebaseProvider');
+  }
+  return context.firebaseApp;
+};
+
+export const useFirestore = () => {
+  const context = useContext(FirebaseContext);
+  if (context === undefined) {
+    throw new Error('useFirestore must be used within a FirebaseProvider');
+  }
+  return context.firestore;
+};
+
+export const useAuth = () => {
+  const context = useContext(FirebaseContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within a FirebaseProvider');
+  }
+  return context.auth;
+};
